723ca65d86a8f0661ec30e88a0d037375e4491a0
KS <55687992+KimSatok@users.noreply.github.com>
2022-03-30 10:57:11 +0300

    Bot commands switched to match case
    Changed bot channel commands from if statement to match case.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot.py
 import os
-
 import discord
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +9,14 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
+
+#botcommand block
+botprefix = '!'
+def botcurrentprefix(): return botprefix + 'prefix'
+def botsetprefix(): return botprefix + 'set'
+def botgreet(): return botprefix + 'greet'
+def bothelp(): return botprefix + 'help'
+#end of botcommand block
 
 @client.event
 async def on_ready():
@@ -23,9 +31,29 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if message.content.startswith('1'):
-        channel = message.channel
-        m = message.author
-        await channel.send('hello ' + str(m))
+    global botprefix
+    channel = message.channel
+    mauth = message.author
+    match message:
+        case botcommand if botcommand.content.startswith(botcurrentprefix()) == True: #botcurrent prefix
+            await channel.send('Current botcommand prefix is ' + str(botprefix))
+
+        case botcommand if botcommand.content.startswith(botgreet()) == True: #botgreet
+            await channel.send('Hello ' + str(mauth))
+
+        case botcommand if botcommand.content.startswith(botsetprefix()) == True: #botsetprefix
+            msg = message.content
+            botprefix = msg[-1]
+            await channel.send('New botcommand prefix is ' +str(botprefix))
+            return botprefix
+
+        case botcommand if botcommand.content.startswith(bothelp()) == True: #bothelp
+            await channel.send(
+                'Current commands for the bot are: \n' +
+                botcurrentprefix() + '\n' +
+                botsetprefix() + '\n' +
+                botgreet() + '\n' +
+                bothelp() + '\n'
+            )
 
 client.run(TOKEN)
