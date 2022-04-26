024d2578e813b7492fab97b011a51458de64a793
KS <55687992+KimSatok@users.noreply.github.com>
2022-04-26 20:16:27 +0300

    error handling and some improvements

--- commands/ytl.py
+++ commands/ytl.py
@@ -1,5 +1,5 @@
 # YouTube Link
-import json, os, discord, requests
+import json, os, discord, requests, traceback
 
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
@@ -51,16 +51,16 @@ class YoutubeWatcher(commands.Cog):
     @commands.command()
     async def setyoutubeid(self, ctx, *, youtuberid):
 
-        url = f"https://www.googleapis.com/youtube/v3/search?key={YTKEY}&channelId={youtuberid}&part=id&order=date"
+        url = f"https://www.googleapis.com/youtube/v3/search?key={YTKEY}&channelId={youtuberid}&part=snippet"
         print(url) #printing the url for test purposes
         try:
             json_url = requests.get(url)
             data = json.loads(json_url.text)
-            vid_data = data["items"] #checking if json has items data to see if the channel exists or not.In case there is no items the channel is not valid
+            channelname = data['items'][0]['snippet']['channelTitle']
             self.youtubeid = youtuberid
-            await ctx.send(f"Youtuber ID set to ``{self.youtubeid}``")
+            await ctx.send(f"Youtuber ID set to ``{self.youtubeid}`` \nChannel name is {channelname}")
         except:
-            await ctx.send("Invalid channel")
+            await ctx.send("Invalid channel or the channel is empty")
 
     #command to set the discord channel where bot should post new videos
     @commands.command()
@@ -77,7 +77,7 @@ class YoutubeWatcher(commands.Cog):
     @commands.command()
     async def checktest(self, ctx):
         if (self.channel != None and self.youtubeid != None):
-            videolist = self.fetch_channel_videos(10)
+            videolist = self.fetch_channel_videos(1)
             newvideo = "https://www.youtube.com/watch?v=" + videolist[0]
             await ctx.send(f"Video ID list ``{videolist}``")
             if newvideo == self.oldvid:
@@ -94,21 +94,34 @@ class YoutubeWatcher(commands.Cog):
             await ctx.send(f"Discord channel & youtube channel ID not set.")
     
     #background loop for checking target channel for new video uploads
-    @tasks.loop(seconds=60)
+    @tasks.loop(minutes=15)
     async def ytupdates(self):
-        videolist = self.fetch_channel_videos(10)
-        newvideo = "https://www.youtube.com/watch?v=" + videolist[0]
-        if newvideo == self.oldvid:
-            return
-        else:
-            self.oldvid = newvideo
-            await self.channel.send(f"Hello there. {newvideo}")
+        try:
+            videolist = self.fetch_channel_videos(1)
+            newvideo = "https://www.youtube.com/watch?v=" + videolist[0]
+            if newvideo == self.oldvid:
+                return
+            else:
+                self.oldvid = newvideo
+                await self.channel.send(f"Hello there. {newvideo}")
+        except Exception:
+            print("There was an error")
+            print(traceback.format_exc())
+            
 
     #user command for starting the channel monitoring loop
     @commands.command()
     async def start(self, ctx):
-        self.ytstart()
-        await ctx.send("Starting monitoring")
+        if self.channel != None and self.youtubeid != None:
+            self.ytstart()
+            await ctx.send("Starting monitoring")
+        elif self.channel != None:
+            await ctx.send(f"Youtube channel ID not set.")
+        elif self.youtubeid != None:
+            await ctx.send(f"Discord channel not set.")
+        else:
+            await ctx.send(f"Discord channel & youtube channel ID not set.")
+
 
     #user command for stopping the channel monitoring loop
     @commands.command()
